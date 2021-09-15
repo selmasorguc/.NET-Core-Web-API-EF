8f4ca51721e5dad80f576d6bdc0a4b6b72a2e76b
selmaso <selmaso@maestralsolutions.com>
2021-09-15 09:30:39 +0200

    Models add

---
+++ Models/Character.cs
@@ -0,0 +1,22 @@
+namespace Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public int HitPoints { get; set; }
+        
+        public int Strength { get; set; }
+        
+        public int Defense { get; set; }
+        
+        public int Intelligence { get; set; }
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+        
+        
+        
+        
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public enum RpgClass
+    {
+        Knight, 
+        Mage,
+        Cleric
+    }
+}
